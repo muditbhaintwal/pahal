fe4a6425e9046d2f33408a51caba16dd582724ea
mudit bhaintwal <9479423+muditbhaintwal@users.noreply.github.com>
2024-07-20 19:19:45 +0530

    added 2 services

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM openjdk:21-jre-slim
-COPY build/libs/pahal-0.0.1-SNAPSHOT.jar /usr/app/pahal.jar
-WORKDIR /usr/app
-ENTRYPOINT ["java", "-jar", "pahal.jar"]
---
+++ saksatkaar/src/main/java/com/funapp/PahalApplication.java
@@ -0,0 +1,14 @@
+package com.funapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PahalApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome from Pahal!!!");
+		SpringApplication.run(PahalApplication.class, args);
+	}
+
+}
